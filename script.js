0a0bd0cdbe18d7624df3d4a72340117051ef817d
zenix-tv <mtobarziege@gmail.com>
2026-04-27 18:50:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("ZENIX funcionando 🚀");
+const track = document.querySelector('.slider-track');
+
+let paused = false;
+
+track.addEventListener('click', () => {
+  paused = !paused;
+  track.style.animationPlayState = paused ? 'paused' : 'running';
+});
